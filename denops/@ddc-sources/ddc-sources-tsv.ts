7efad2d259c044b6f3a7abbbb4223490e09aab27
Dzzt <parifuransu@gmail.com>
2023-04-25 14:41:11 +0900

    second commit

--- denops/@ddc-sources/ddc-sources-tsv.ts
+++ denops/@ddc-sources/ddc-sources-tsv.ts
@@ -4,6 +4,7 @@ import {
   Item,
   SourceOptions,
 } from "https://deno.land/x/ddc_vim@v3.4.0/types.ts";
+
 import {
   Denops,
 } from "https://deno.land/x/ddc_vim@v3.4.0/deps.ts";
@@ -11,16 +12,16 @@ import {
 type Params = Record<never, never>;
 
 export class Source extends BaseSource<Params> {
-  override async gather(args: {
-    denops: Denops;
-    options: DdcOptions;
-    sourceOptions: SourceOptions;
-    sourceParams: Params;
-    completeStr: string;
-  }): Promise<Item[]> {
-    return [
-        { word: "foo"}, { word: "bar"}, { word: "baz"},
-    ];
+  override async gather(
+      args: {
+        denops: Denops;
+        options: DdcOptions;
+        sourceOptions: SourceOptions;
+        sourceParams: Params;
+        completeStr: string;
+      }
+  ): Promise<Item[]> {
+    return [ { word: "foo"}, { word: "bar"}, { word: "baz"}, ];
   }
 
   override params(): Params {
